9fa9a1f2b3f5857e76e7e5a800ad9cf24af60ac2
Marcelo <marcelo.adrian2011@fpuna.edu.py>
2023-11-18 10:23:23 -0300

    Ajout de nouvelles funcionatilites

---
+++ src/main/java/com/learningapp/vocablearner/VocabularyLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.learningapp.vocablearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyLearnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyLearnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningapp/vocablearner/VocabularyLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningapp.vocablearner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyLearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
